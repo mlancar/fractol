5d518a46b84e98f503515cb8afa639faa0757566
Marine LANCAR <malancar@e1r4p23.clusters.42paris.fr>
2023-03-28 07:02:12 +0200

    close window

--- event.c
+++ event.c
@@ -6,23 +6,24 @@
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:07:45 by malancar          #+#    #+#             */
-/*   Updated: 2023/03/28 06:40:43 by malancar         ###   ########.fr       */
+/*   Updated: 2023/03/28 06:57:49 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx/mlx.h"
 #include <stdio.h>
 
-int	close(void *win, void *mlx)
+int	close(void *mlx)
 {
-	mlx_destroy_window(mlx, win);
+	printf("coucou\n");
+	mlx_loop_end(mlx);
 	return (0);
 }
 
-int	key_hook(int keycode, void *mlx, void *win)
+int	key_hook(int keycode, void *mlx)
 {
 	if (keycode == 113)
-		close(win, mlx);
+		close(mlx);
 	return (0);
 }
 
@@ -33,7 +34,8 @@ int main()
 	
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 640, 480, "Hello world!");
-	mlx_key_hook(win, key_hook, &mlx);
+	mlx_key_hook(win, key_hook, mlx);
 	mlx_hook(win, 113, 1L<<0, close, &mlx);
 	mlx_loop(mlx);
+	mlx_destroy_window(mlx, win);
 }
