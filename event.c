1e9431cd53b9d9db271aa2b0b3110f0b94e1b3e5
Marine LANCAR <malancar@e1r1p12.clusters.42paris.fr>
2023-04-22 16:54:24 +0200

    zoom position souris

--- event.c
+++ event.c
@@ -6,7 +6,7 @@
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:07:45 by malancar          #+#    #+#             */
-/*   Updated: 2023/04/21 18:53:31 by malancar         ###   ########.fr       */
+/*   Updated: 2023/04/22 16:53:41 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,48 +55,43 @@ int	key_hook(int key_code, t_graph *var)
 
 int	mouse_hook(int mouse_code, int x, int y, t_graph *var)
 {
-	printf("x = %d\ny = %d\n", x, y);
 	if (mouse_code == 5)
 	{
-		var->zoom = 0.9;
-		var->x_min = var->x_min / var->zoom;
-		var->x_max = var->x_max / var->zoom;
-		var->y_min = var->y_min / var->zoom;
-		var->y_max = var->y_max / var->zoom;
+		var->zoom = 1.1;
+		double	d_x;
+		double	d_y;
+		
+		d_x = 0;
+		d_y = 0;
+		d_x = (var->x_max - var->x_min) * (1 - var->zoom);
+		d_y = (var->y_max - var->y_min) * (1 - var->zoom);
+		var->x_min = var->x_min + ((double)x / var->win_width) * (2 * d_x);
+		var->x_max = var->x_max - (1 - ((double)x / var->win_width)) * (2 * d_x);
+		var->y_min = var->y_min + (1 - (double)y / var->win_height) * (2 * d_y);
+		var->y_max = var->y_max - (((double)y / var->win_height)) * (2 * d_y);
 		draw_fractal(var);
 	}
 	if (mouse_code == 4)
 	{
+		double	d_x;
+		double	d_y;
+		
 		var->zoom = 0.9;
-		/*var->x_min = var->x_min * var->zoom;
-		var->x_max = var->x_max * var->zoom;
-		var->y_min = var->y_min * var->zoom;
-		var->y_max = var->y_max * var->zoom;*/
-		var->x_min =  (var->x_min - var->x_max) * (x / var->img_width) * (var->zoom);
-		var->x_max = (var->x_max - var->x_max) * (x / var->img_width) * (var->zoom);
-		var->y_min = (var->y_min - var->y_min) * (y / var->img_height) * (var->zoom);
-		var->y_max = (var->y_max - var->y_max) * (y / var->img_height) * (var->zoom);
+		d_x = 0;
+		d_y = 0;
+		d_x = (var->x_max - var->x_min) * (1 - var->zoom);
+		d_y = (var->y_max - var->y_min) * (1 - var->zoom);
+		var->x_min = var->x_min + ((double)x / var->win_width) * (2 * d_x);
+		var->x_max = var->x_max - (1 - ((double)x / var->win_width)) * (2 * d_x);
+		var->y_min = var->y_min + (1 - (double)y / var->win_height) * (2 * d_y);
+		var->y_max = var->y_max - (((double)y / var->win_height)) * (2 * d_y);
 		draw_fractal(var);
 	}
-	
 	return (0);
 }
-/*int	mouse_position(int mouse_pos, int x, int y, t_graph *var)
-{
-	printf("x = %d\ny = %d\n", x, y);
-	if (x < var->img_width && y < var->img_height)
-	{
-		var->x_min =  (x / var->img_width) * var->zoom;
-		var->x_max = (x / var->img_width) * var->zoom;
-		var->y_min = (y / var->img_height) * var->zoom;
-		var->y_max = (y / var->img_height) * var->zoom;
-	}
-	return (0);
-}*/
 
 int	close_window(t_graph *var)
 {
 	mlx_loop_end(var->mlx);
 	return (0);
 }
-
